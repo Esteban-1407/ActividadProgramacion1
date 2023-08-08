e001264f478373fb378fe5168037d6a524e158e4
Esteban-1407 <esalazar2222@cue.edu.co>
2023-08-07 19:47:25 -0500

    Ejercicios con variables privadas #1

--- src/Ejercicios_Algoritmos/Ejercicio2/Ejercicio5/Sticker.java
+++ src/Ejercicios_Algoritmos/Ejercicio2/Ejercicio5/Sticker.java
@@ -3,23 +3,29 @@ package Ejercicios_Algoritmos.Ejercicio2.Ejercicio5;
 public class Sticker {
     public int size;
     public int amount;
-    public void ShowOption(){
-        if(amount > 10){
-            switch (size){
-                case 1 :{
+
+
+
+    public void ShowOption() {
+        if (amount > 10) {
+            switch (size) {
+                case 1: {
                     int price = amount * 4000;
-                    System.out.println("El valor total es de " +price + " pesos");
+                    System.out.println("El valor total es de " + price + " pesos");
                     break;
                 }
-                case 2 :{
+                case 2: {
                     int price = amount * 6000;
                     System.out.println("El valor total es de " + price + " pesos");
+                    break;
+                }
+                default: {
+                    System.out.println("Tamaño de sticker no válido");
                 }
             }
-        }
-        else {
-            System.out.println("Opcion invalida");
-            }
+        } else {
+            System.out.println("Cantidad de stickers no válida");
         }
     }
 
+}
